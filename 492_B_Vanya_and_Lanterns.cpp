a0fe18c79d54e9b65adcdff2a552515c164a9487
Dip Roy <diproyf87@gmail.com>
2024-05-10 02:58:55 -0300

    solved

---
+++ 492_B_Vanya_and_Lanterns.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int n,l;
+    cin>>n>>l;
+
+    int ar[n];
+    for(int i=0; i<n; i++){
+        cin>>ar[i];
+    }
+    sort(ar, ar+n);
+    int mx = 0;
+    for(int i=0; i<n-1; i++){
+        if(ar[i+1] - ar[i] > mx){
+            mx = ar[i+1] - ar[i];
+        }
+    }
+
+    double x = ar[0];
+    double y = double(mx)/2;
+    double ans = max(x,y);
+    double z = l - ar[n-1];
+    ans = max(z, ans);
+
+    cout<<fixed<<setprecision(10)<<ans;
+}
